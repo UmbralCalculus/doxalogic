3d0753cb50eeb3e95c4f9297bb1eb444f7fc127c
umbralcalculus <eric.lindahl@gmail.com>
2015-05-13 22:44:59 +0000

    template typescript express

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="polymer/polymer.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="node/node.d.ts" />
